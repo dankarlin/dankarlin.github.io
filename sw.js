848c209b4ae522569c18f51c50e7aafb645656a5
Dan Karlin <dankarlin@gmail.com>
2025-06-13 10:54:16 -0400

    Updates

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'dankarlin-v1.0.0';
+const urlsToCache = [
+  '/',
+  '/manifest.json',
+  '/dan-karlin-photo.webp',
+  '/dan-karlin-photo.jpg',
+  '/vite.svg',
+  '/sitemap.xml',
+  '/robots.txt'
+];
+
+// Install event - cache resources
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Fetch event - serve from cache when possible
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
